9f35a67155e5342edb52d4ad674665eef7bb3e3b
Hussaini Muhammad Auwal <hauwal4969@gmail.com>
2022-03-31 19:03:50 +0100

    access control added

--- contracts/SportPredictionTreasury.sol
+++ contracts/SportPredictionTreasury.sol
@@ -1,17 +1,20 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/ISportPredictionTreasury.sol";
 
 // A Smart-contract that holds the sport prediction funds
-contract SportPredictionTreasury is Ownable, ISportPredictionTreasury{
+contract SportPredictionTreasury is AccessControl, ISportPredictionTreasury{
     using SafeERC20 for IERC20;
 
     // other ERC20 token address
     IERC20 internal token; 
+
+    //sport prediction address
+    address internal sportPrediction;
 
     // Event triggered once an address deposited in the contract
     event Deposit(address indexed user, uint amount);
@@ -19,6 +22,40 @@ contract SportPredictionTreasury is Ownable, ISportPredictionTreasury{
     // Event triggered once an address withdrawn in the contract
     event Withdraw(address indexed user, uint amount);
 
+    // Emitted when sport prediction address is set
+    event SportPredictionSet( address _address);
+
+    // Restricted to authorised accounts.
+    modifier onlyAuthorized() {
+        require(isAuthorized(msg.sender), 
+        "SportPredictionTreasury:Restricted to only authorized accounts.");
+        _;
+    }
+
+
+    constructor(){
+        _setupRole("deployer", msg.sender); 
+    }
+
+
+    function isAuthorized(address account)
+        public view returns (bool)
+    {
+        if(hasRole("deployer",account)) return true;
+
+        else if(hasRole("sportPrediction", account)) return true;
+
+        return false;
+    }
+
+    function setSportPredictionAddress(address _address)
+        external 
+        onlyRole("deployer")
+    {
+        sportPrediction = _address;
+        _grantRole("sportPrediction", sportPrediction);
+        emit SportPredictionSet(_address);
+    }
 
     /**
      * @notice deposit bnb
@@ -49,7 +86,7 @@ contract SportPredictionTreasury is Ownable, ISportPredictionTreasury{
      * @notice withdraw bnb
      * @param _amount the withdrawal amount
      */
-    function withdraw(uint _amount) public override onlyOwner{
+    function withdraw(uint _amount) public override onlyRole("deployer"){
         payable(msg.sender).transfer(_amount);
         emit Withdraw(msg.sender, _amount);
     }
@@ -62,7 +99,8 @@ contract SportPredictionTreasury is Ownable, ISportPredictionTreasury{
      */
     function withdrawToken(address _token, address _to, uint _amount) 
         public 
-        override{
+        override
+        onlyAuthorized{
         token = IERC20(_token);
         token.safeTransfer(_to, _amount);
         emit Withdraw(_to, _amount);
